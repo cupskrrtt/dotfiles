761daf229788b1f38cd4a2671b3414334bc98819
Luthfi Khan <khanalfarizzi@hotmail.com>
2025-04-10 17:20:53 +0700

    arch-commit

--- nvim/.config/nvim/lua/plugins/nitpick.lua
+++ nvim/.config/nvim/lua/plugins/nitpick.lua
@@ -1,11 +1,13 @@
 return {
-  "cupskrrtt/nitpick",
-  config = function()
-    require("nitpick").setup({
-      linter_by_ft = {
-        typescriptreact = { "eslint_d", "eslint", "biome" },
-        javascript = { "eslint_", "eslint" },
-      },
-    })
-  end,
+	"cupskrrtt/nitpick",
+	config = function()
+		require("nitpick").setup({
+			linter_by_ft = {
+				typescriptreact = { "eslint_d", "eslint", "biome" },
+				typescript = { "eslint_d", "eslint", "biome" },
+				javascript = { "eslint_d", "eslint", "biome" },
+				javascriptreact = { "eslint_d", "eslint", "biome" },
+			},
+		})
+	end,
 }
