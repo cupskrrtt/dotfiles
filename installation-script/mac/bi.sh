b43c6819b208577b01937d1f6e2812b14480d085
Luthfi Khan <cup@Luthfis-MacBook-Air.local>
2025-05-19 03:23:31 +0700

    yalip

--- installation-script/mac/bi.sh
+++ installation-script/mac/bi.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+brew tap oven-sh/bun
+
 brew install neovim
 brew install alacritty
 brew install stow
@@ -7,6 +9,17 @@ brew install zsh-syntax-highlighting
 brew install --cask font-jetbrains-mono-nerd-font
 brew install vscode-langservers-extracted
 brew install typescript-language-server
+brew install tailwindcss-language-server
 brew install eza
 brew install starship
 brew install discord
+brew install firefox
+brew install --cask google-chrome
+brew install node
+brew install bun
+brew install tmux
+brew install --cask dbeaver-community
+brew install supabase/tap/supabase
+brew install nmap
+brew install uv
+brew install pyright
